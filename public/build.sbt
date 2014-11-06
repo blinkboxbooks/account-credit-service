bf06e698e54cd5f8627b0d72e9655fb44063871a
Tom <thomasf@blinkbox.com>
2014-11-06 12:37:26 +0000

    Host/IP should be read from config

--- public/build.sbt
+++ public/build.sbt
@@ -2,6 +2,7 @@ name := "account-credit-service-v2-public"
 
 libraryDependencies ++= {
   Seq(
-    "com.blinkbox.books" %% "common-spray" % "0.17.5"
+    "com.blinkbox.books" %% "common-spray" % "0.17.5",
+    "com.blinkbox.books" %% "common-config" % "1.4.1"
   )
 }
--- public/src/main/scala/com/blinkbox/books/credit/Main.scala
+++ public/src/main/scala/com/blinkbox/books/credit/Main.scala
@@ -2,6 +2,7 @@ package com.blinkbox.books.credit
 
 import akka.actor.{ActorRefFactory, ActorSystem, Props}
 import akka.util.Timeout
+import com.blinkbox.books.config.Configuration
 import com.blinkbox.books.spray.{HealthCheckHttpService, HttpServer}
 import spray.can.Http
 import spray.http.Uri.Path
@@ -9,10 +10,14 @@ import spray.routing.HttpServiceActor
 
 import scala.concurrent.duration._
 
-object Main extends App {
+object Main extends App with Configuration {
   val system = ActorSystem("account-credit-service-v2-public")
   val service = system.actorOf(Props(classOf[PublicApiActor]))
-  HttpServer(Http.Bind(service, interface = "127.0.0.1", port=9876))(system, system.dispatcher, Timeout(10.seconds))
+
+  val interface = config.getString("admin.listen.address")
+  val port = config.getInt("admin.listen.port")
+
+  HttpServer(Http.Bind(service, interface = interface, port=port))(system, system.dispatcher, Timeout(10.seconds))
 }
 
 class PublicApiActor extends HttpServiceActor {
