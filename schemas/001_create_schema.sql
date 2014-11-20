6ee2e4e8da3914a28b3fe36d0d68f92be70e1f41
mohameda <mohameda@blinkbox.com>
2014-11-20 11:48:47 +0000

    fixed broken db script

--- schemas/001_create_schema.sql
+++ schemas/001_create_schema.sql
@@ -7,47 +7,47 @@ SET @jira_issue= 'CRED-27';
 DROP TABLE IF EXISTS transaction_type ;
 
 CREATE TABLE transaction_type (
-  pk_transaction_type TINYINT UNSIGNED NOT NULL,
-  description VARCHAR(50) NOT NULL,
-  PRIMARY KEY (pk_transaction_type )
+  transaction_type_id TINYINT UNSIGNED NOT NULL,
+  type VARCHAR(50) NOT NULL,
+  PRIMARY KEY (transaction_type_id )
 ) ENGINE = InnoDB, DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ;
 
 DROP TABLE IF EXISTS reason ;
 CREATE TABLE reason (
-  pk_reason TINYINT UNSIGNED NOT NULL,
-  description VARCHAR(50) NOT NULL,
-  PRIMARY KEY (pk_reason)
+  reason_id TINYINT UNSIGNED NOT NULL,
+  type VARCHAR(50) NOT NULL,
+  PRIMARY KEY (reason_id)
 ) ENGINE = InnoDB, DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ;
 
 DROP TABLE IF EXISTS credit_balance ;
 CREATE TABLE credit_balance (
- pk_credit_balance INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+ credit_balance_id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
  request_id VARCHAR(55) NOT NULL COMMENT 'unique identifier to make sure credit/debit gets applyied only once',
  value FLOAT(7,2) NOT NULL,
- pk_transaction_type TINYINT  NOT NULL,
- pk_reason TINYINT ,
+ transaction_type_id TINYINT UNSIGNED NOT NULL,
+ reason_id TINYINT UNSIGNED,
  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
  customer_id INT(11) NOT NULL,
  admin_user_id INT(11) ,
  admin_user_role VARCHAR(11) NOT NULL COMMENT 'Admin user Role at the time of this transaction',
- PRIMARY KEY (pk_credit_balance),
- FOREIGN KEY `fk_credit_balance_transaction_type` (`pk_transaction_type`) references `transaction_type` (`pk_transaction_type`),
- FOREIGN KEY `fk_credit_balance_reason` (`pk_reason`) references `reason` (`pk_reason`)
+ PRIMARY KEY (credit_balance_id),
+ FOREIGN KEY `fk_credit_balance_transaction_type` (`transaction_type_id`) references `transaction_type` (`transaction_type_id`),
+ FOREIGN KEY `fk_credit_balance_reason` (`reason_id`) references `reason` (`reason_id`)
  ) ENGINE = InnoDB, DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ;
 
 
-INSERT INTO transaction_type (`pk_transaction_type`, `description`) VALUES ('0', 'Credit');
-INSERT INTO transaction_type (`pk_transaction_type`, `description`) VALUES ('1', 'Debit');
+INSERT INTO transaction_type (`transaction_type_id`, `type`) VALUES ('0', 'Credit');
+INSERT INTO transaction_type (`transaction_type_id`, `type`) VALUES ('1', 'Debit');
 
-INSERT INTO reason (`pk_reason`, `description`) VALUES ('0', 'GoodwillBookIssue');
-INSERT INTO reason (`pk_reason`, `description`) VALUES ('1', 'GoodwillTechnicalIssue');
-INSERT INTO reason (`pk_reason`, `description`) VALUES ('2', 'GoodwillServiceIssue');
-INSERT INTO reason (`pk_reason`, `description`) VALUES ('3', 'GoodwillCustomerRetention');
-INSERT INTO reason (`pk_reason`, `description`) VALUES ('4', 'CreditRefund');
-INSERT INTO reason (`pk_reason`, `description`) VALUES ('5', 'StaffCredit');
-INSERT INTO reason (`pk_reason`, `description`) VALUES ('6', 'CreditVoucherCode');
-INSERT INTO reason (`pk_reason`, `description`) VALUES ('7', 'Hundle2Promotion');
+INSERT INTO reason (`reason_id`, `type`) VALUES ('0', 'GoodwillBookIssue');
+INSERT INTO reason (`reason_id`, `type`) VALUES ('1', 'GoodwillTechnicalIssue');
+INSERT INTO reason (`reason_id`, `type`) VALUES ('2', 'GoodwillServiceIssue');
+INSERT INTO reason (`reason_id`, `type`) VALUES ('3', 'GoodwillCustomerRetention');
+INSERT INTO reason (`reason_id`, `type`) VALUES ('4', 'CreditRefund');
+INSERT INTO reason (`reason_id`, `type`) VALUES ('5', 'StaffCredit');
+INSERT INTO reason (`reason_id`, `type`) VALUES ('6', 'CreditVoucherCode');
+INSERT INTO reason (`reason_id`, `type`) VALUES ('7', 'Hundle2Promotion');
 
 
 #######################################################################################################################
