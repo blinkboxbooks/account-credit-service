8b1eff9157c964b0090364c3d4b49adbf59cec0d
mohameda <mohameda@blinkbox.com>
2014-11-20 23:11:37 +0000

    added request schema

--- admin/src/main/scala/com/blinkbox/books/credit/admin/AdminApiRequests.scala
+++ admin/src/main/scala/com/blinkbox/books/credit/admin/AdminApiRequests.scala
@@ -1,22 +1,14 @@
 package com.blinkbox.books.credit.admin
 
-
 import org.joda.time.DateTime
 
 case class AddCreditRequest(
-  requestId: String, 
-  value: BigDecimal, 
-  action: Int, 
-  attributes: List[CampaignAttributeRequest], 
-  startDate: DateTime, 
-  endDate: Option[DateTime], 
-  redemptionLimit: Option[Int], 
-  initialVoucherCount: Option[Int]
-) {
-  AddCreditRequest.this match {
-    case AddCreditRequest(_, _, _, _, startDate, Some(endDate), _, _) if endDate.isBefore(startDate) =>
-      throw new ConstraintViolation("endDate", "endDate must not be before startDate")
-    case _ =>
-  }
+  requestId: String,
+  value: BigDecimal,
+  reason: Reason.Reason)
+
+object Reason extends Enumeration {
+  type Reason = Value
+  val GoodwillBookIssue, GoodwillTechnicalIssue, GoodwillServiceIssue, GoodwillCustomerRetention, CreditRefund, StaffCredit, CreditVoucherCode, Hudl2Promotion = Value
 }
 
