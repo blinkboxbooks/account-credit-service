d4dcc998eff30d78f8d34f217763519898049a2d
mohameda <mohameda@blinkbox.com>
2014-11-27 17:13:20 +0000

    implemented Db mapping suggestions

--- common/src/main/scala/com/blinkbox/books/credit/db/AccountCreditTables.scala
+++ common/src/main/scala/com/blinkbox/books/credit/db/AccountCreditTables.scala
@@ -11,26 +11,22 @@ import TransactionType._
 trait AccountCreditTables[Profile <: JdbcProfile] extends TablesContainer[Profile] {
   import driver.simple._
 
-  // TODO - how to make these generic? Note that the driver instance *has* to be implicitly available for this to work
   implicit lazy val reasonMapper = MappedColumnType.base[Reason.Reason, Int](_.id, Reason.apply)
   implicit lazy val transactionTypeMapper = MappedColumnType.base[TransactionType.TransactionType, Int](_.id, TransactionType.apply)
-  implicit lazy val timestampMapper = MappedColumnType.base[DateTime, Timestamp](
-    dateTime => new Timestamp(dateTime.getMillis),
-    timestamp => new DateTime(timestamp).toDateTime(DateTimeZone.UTC))
-
+  
   val creditBalance = TableQuery[CreditBalanceTable]
 
   class CreditBalanceTable(tag: Tag) extends Table[CreditBalance](tag, "credit_balance") {
     def id = column[Int]("credit_balance_id", O.PrimaryKey, O.AutoInc)
-    def requestId = column[String]("request_id", O.NotNull)
-    def value = column[BigDecimal]("value", O.DBType("decimal(7, 2)"), O.NotNull)
-    def transactionType = column[TransactionType]("transaction_types_id", O.NotNull)
-    def reason = column[Reason]("reasons_id ", O.Nullable)
-    def createdAt = column[DateTime]("created_at", O.NotNull)
-    def updatedAt = column[DateTime]("updated_at", O.Nullable)
+    def requestId = column[String]("request_id")
+    def value = column[BigDecimal]("value", O.DBType("decimal(7, 2)"))
+    def transactionType = column[TransactionType]("transaction_types_id")
+    def reason = column[Option[Reason]]("reasons_id ")
+    def createdAt = column[DateTime]("created_at")
+    def updatedAt = column[Option[DateTime]]("updated_at")
     def customerId = column[Int]("customer_id", O.NotNull)
-    def adminUserId = column[Int]("admin_user_id", O.Nullable)
-    def * = (id.?, requestId, value, transactionType, reason.?, createdAt, updatedAt.?, customerId, adminUserId.?) <> (CreditBalance.tupled, CreditBalance.unapply _)
+    def adminUserId = column[Option[Int]]("admin_user_id")
+    def * = (id.?, requestId, value, transactionType, reason, createdAt, updatedAt, customerId, adminUserId) <> (CreditBalance.tupled, CreditBalance.unapply _)
   }
 }
 
