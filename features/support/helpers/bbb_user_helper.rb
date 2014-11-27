d1f02f2b607e917bbf00eaaea3d1c11407fe911b
Daniel Lam <daniell@blinkbox.com>
2014-11-27 12:13:38 +0000

    code clean up of bbb_user_helper.rb

--- features/support/helpers/bbb_user_helper.rb
+++ features/support/helpers/bbb_user_helper.rb
@@ -1,54 +1,42 @@
 module KnowsAboutBbbUsers
-  @@csm_user = Blinkbox::User.new(:username => csm_username, :password => csm_password)
-  @@csm_user.authenticate
-
-  @@csr_user = Blinkbox::User.new(:username => csr_username, :password => csr_password)
-  @@csr_user.authenticate
-
-  @@api_user = Blinkbox::User.new(:username => csr_username, :password => csr_password)
-  @@api_user.authenticate
-
-  @@last_admin_user
-  @@last_public_user
+  attr_accessor :last_admin_user, :last_public_user
 
   def use_api_user
-    @@api_user
+    @last_admin_user = authenticate_user(api_username, api_password)
   end
 
   def use_csm_user
-    @@last_admin_user = @@csm_user
+    @last_admin_user = authenticate_user(csm_username, csm_password)
   end
 
   def use_csr_user
-    @@last_admin_user = @@csr_user
+    @last_admin_user = authenticate_user(csr_username, csr_password)
   end
 
   def new_public_user
     public_user = Blinkbox::User.new(:username => random_email, :password => 'abc123')
     public_user.register
     public_user.authenticate
-    @@last_public_user = public_user
+    @last_public_user = public_user
   end
 
   def use_unknown_user
-    @@last_public_user = Blinkbox::User.new(:username => 'unknown-user@bbb.com', :password => 'abc123')
-    @@last_public_user.instance_eval %Q"
+    @last_public_user = Blinkbox::User.new(:username => 'unknown-user@bbb.com', :password => 'abc123')
+    @last_public_user.instance_eval %Q"
       def user_id
         '999999'
       end
     "
   end
 
-  def last_admin_user
-    @@last_admin_user
-  end
-
-  def last_public_user
-    @@last_public_user
-  end
-
   def user_id_of(user)
     /\d+/.match(user.user_id).to_s
+  end
+
+  def authenticate_user(username, password)
+    user = Blinkbox::User.new(:username => username, :password => password)
+    user.authenticate
+    user
   end
 end
 World(KnowsAboutBbbUsers)
