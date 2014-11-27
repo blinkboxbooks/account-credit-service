fe28a9ad1b98bbd4565cde32e99a9b180f1d328a
Tom <thomasf@blinkbox.com>
2014-11-27 18:24:10 +0000

    Revert "CRED-38: Refactor `pathPrefix { pathEnd` to `path`"
    This was a breaking change.
    
    This reverts commit 0bea52d9681bfaf94b4130565fa0c56d1aa543e1.

--- admin/src/main/scala/com/blinkbox/books/credit/admin/AdminApi.scala
+++ admin/src/main/scala/com/blinkbox/books/credit/admin/AdminApi.scala
@@ -28,13 +28,15 @@ class AdminApi(creditHistoryRepository: CreditHistoryRepository, authenticator: 
 
   val route = monitor(logger, throwableMarshaller) {
     pathPrefix("admin" / "users" / IntNumber) { userId =>
-      path("accountcredit") {
-        get {
-          authenticateAndAuthorize(authenticator, hasAnyRole(CustomerServicesRep, CustomerServicesManager)) { adminUser =>
-            val issuerBehaviour = if (adminUser.isInRole(UserRole.CustomerServicesManager)) keepIssuer _ else removeIssuer _
-            complete(creditHistoryRepository.lookupCreditHistoryForUser(userId).map {
-              case CreditHistory(m, h) => CreditHistoryForRendering(m, h.map(issuerBehaviour))
-            })
+      pathPrefix("accountcredit") {
+        pathEnd {
+          get {
+            authenticateAndAuthorize(authenticator, hasAnyRole(CustomerServicesRep, CustomerServicesManager)) { adminUser =>
+              val issuerBehaviour = if (adminUser.isInRole(UserRole.CustomerServicesManager)) keepIssuer _ else removeIssuer _
+              complete(creditHistoryRepository.lookupCreditHistoryForUser(userId).map {
+                case CreditHistory(m, h) => CreditHistoryForRendering(m, h.map(issuerBehaviour))
+              })
+            }
           }
         } ~
         path("debits") {
