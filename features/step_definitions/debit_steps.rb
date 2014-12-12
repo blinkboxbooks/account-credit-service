1b6f5c9f27e43cdf42cc65be1e5ab78a0c468923
Daniel Lam <daniell@blinkbox.com>
2014-12-12 11:41:46 +0000

    correcting step def regex for amounts

--- features/step_definitions/debit_steps.rb
+++ features/step_definitions/debit_steps.rb
@@ -1,4 +1,4 @@
-Given(/^I (?:try to )?debit the customer ([\d.]+) in GBP( using the same requestId as before)?$/) do |amount_debit, use_last_request_id|
+Given(/^I (?:try to )?debit the customer ([\d.-]+) in GBP( using the same requestId as before)?$/) do |amount_debit, use_last_request_id|
   request_id = use_last_request_id ? last_request_id : new_request_id
   post_admin_account_debit(last_admin_user.access_token, amount_debit, user_id_of(last_public_user), request_id)
 end
