4b9e9931c3ea85a6a6e0b1b85cebd5a085040509
Daniel Lam <daniell@blinkbox.com>
2014-12-15 17:07:24 +0000

    Removed space about captured param. Fixed reason assertion.

--- features/step_definitions/reason_list_steps.rb
+++ features/step_definitions/reason_list_steps.rb
@@ -2,9 +2,9 @@ When(/^I make a request for the reasons list$/) do
   get_reasons_list
 end
 
-Then(/^the reasons returned are:$/) do | table |
+Then(/^the reasons returned are:$/) do |table|
   expected_reasons = table.transpose.raw[0]
   actual_reasons = parse_last_api_response['reasons']
 
-  expect(expected_reasons & actual_reasons).to_be expected_reasons
+  expect(expected_reasons & actual_reasons).to eq(expected_reasons)
 end
