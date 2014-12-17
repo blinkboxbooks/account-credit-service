1c74ccd94f4dd96131bf747b139bb7fa24345dba
Daniel Lam <daniell@blinkbox.com>
2014-12-17 10:23:59 +0000

    making reason list check better

--- features/step_definitions/reason_list_steps.rb
+++ features/step_definitions/reason_list_steps.rb
@@ -6,6 +6,5 @@ Then(/^the reasons returned are:$/) do |table|
   expected_reasons = table.transpose.raw[0]
   actual_reasons = parse_last_api_response['reasons']
 
-  expect(expected_reasons & actual_reasons).to eq(expected_reasons)
-  expect(expected_reasons.size).to eq(actual_reasons.size)
+  expect(actual_reasons.to_set).to eq(expected_reasons.to_set)
 end
